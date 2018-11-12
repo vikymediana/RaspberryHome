b0ae2953a7b36f00c05575756edd70344bb8080e
victoria <vikymediana@gmail.com>
2018-11-12 18:36:57 +0100

    Behaviours

--- src/utils/GpioUtils.java
+++ src/utils/GpioUtils.java
@@ -9,13 +9,13 @@ import java.util.Map;
 public class GpioUtils {
 
     public static Pin getPinByName(String pinName) {
-        Map<String, Pin> pins = new HashMap<>();
-        pins.put("0", RaspiPin.GPIO_00);
-        pins.put("1", RaspiPin.GPIO_01);
-        pins.put("2", RaspiPin.GPIO_02);
+        Map<Integer, Pin> pins = new HashMap<>();
+        pins.put(0, RaspiPin.GPIO_00);
+        pins.put(1, RaspiPin.GPIO_01);
+        pins.put(2, RaspiPin.GPIO_02);
         System.out.println("pinName " + pinName);
-        Pin pin = pins.get(pinName);
-        System.out.println(pins.containsKey(pinName));
+        Pin pin = pins.get(Integer.valueOf(pinName));
+        System.out.println(pins.containsKey(Integer.valueOf(pinName)));
         return pin;
     }
 
