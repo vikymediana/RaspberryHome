206c296583e7e65a0d27babf5018a1498ed52a64
victoria <vikymediana@gmail.com>
2018-11-14 18:29:21 +0100

    LED auto off

--- src/rbhbehaviour/BooleanBehaviour.java
+++ src/rbhbehaviour/BooleanBehaviour.java
@@ -2,42 +2,62 @@ package rbhbehaviour;
 
 import com.pi4j.io.gpio.*;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 
+import java.util.Date;
 import java.util.logging.Level;
 
 public class BooleanBehaviour extends CyclicBehaviour {
 
     GpioController gpioController = GpioFactory.getInstance();
-    Pin gpioPin;
+    Pin pin;
     GpioPinDigitalOutput output;
-    private boolean initValue;
+    private boolean actualStatus;
+    private long timeout;
+    private long lastTrue;
     private Logger myLogger = Logger.getMyLogger(getClass().getName());
 
-    public BooleanBehaviour(Pin gpioPin, boolean initValue) {
-        this.gpioPin = gpioPin;
-        this.initValue = initValue;
+    public BooleanBehaviour(Pin pin, boolean actualStatus, long timeout) {
+        this.pin = pin;
+        this.actualStatus = actualStatus;
+        this.timeout = timeout;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        this.output = gpioController.provisionDigitalOutputPin(gpioPin, "MyLED" + getAgent().getAID(), initValue ? PinState.HIGH : PinState.LOW);
+        this.output = gpioController.provisionDigitalOutputPin(pin, "MyLED" + getAgent().getAID(), actualStatus ? PinState.HIGH : PinState.LOW);
+        getAgent().addBehaviour(new TickerBehaviour(getAgent(), timeout) {
+                                    protected void onTick() {
+                                        if (actualStatus) {
+                                            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                                            msg.setContent(String.valueOf(false));
+                                            msg.addReceiver(getAgent().getAID());
+                                            getAgent().send(msg);
+                                        }
+                                    }
+                                }
+        );
     }
 
     @Override
     public void action() {
 
         ACLMessage msg = getAgent().receive();
-        if (msg!=null) {
-            myLogger.log(Level.INFO, " - " + getAgent().getLocalName() + " <- " + msg.getContent() );
+        if (msg != null) {
+            myLogger.log(Level.INFO, " - " + getAgent().getLocalName() + " <- " + msg.getContent());
             if (msg.getPerformative() == ACLMessage.INFORM) {
                 String content = msg.getContent();
                 if ((content != null) && (content.indexOf("true") != -1)) {
                     this.output.high();
+                    actualStatus = true;
+                    this.lastTrue = new Date().getTime();
                 } else if ((content != null) && (content.indexOf("false") != -1)) {
                     this.output.low();
+                    actualStatus = false;
                 }
             }
         } else {
--- src/rbhbehaviour/MovBehaviour.java
+++ src/rbhbehaviour/MovBehaviour.java
@@ -14,14 +14,14 @@ import java.util.List;
 public class MovBehaviour extends SimpleBehaviour {
 
     final GpioController gpioController = GpioFactory.getInstance();
-    Pin gpioPin;
+    Pin pin;
     GpioPinDigitalInput input;
     private boolean actualStatus;
     private final List<String> dstTypes;
     private Logger myLogger = Logger.getMyLogger(getClass().getName());
 
-    public MovBehaviour(Pin gpioPin, boolean initValue, List<String> dstTypes) {
-        this.gpioPin = gpioPin;
+    public MovBehaviour(Pin pin, boolean initValue, List<String> dstTypes) {
+        this.pin = pin;
         this.dstTypes = dstTypes;
         this.actualStatus = initValue;
     }
@@ -29,7 +29,7 @@ public class MovBehaviour extends SimpleBehaviour {
     @Override
     public void onStart() {
         super.onStart();
-        this.input = gpioController.provisionDigitalInputPin(gpioPin, actualStatus ? PinPullResistance.PULL_UP : PinPullResistance.PULL_DOWN);
+        this.input = gpioController.provisionDigitalInputPin(pin, actualStatus ? PinPullResistance.PULL_UP : PinPullResistance.PULL_DOWN);
     }
 
     @Override
@@ -38,17 +38,18 @@ public class MovBehaviour extends SimpleBehaviour {
         input.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-
                 try {
                     if (event.getState().isHigh() && !actualStatus || event.getState().isLow() && actualStatus) {
-                        List<DFAgentDescription> dstAgents = DFUtils.findAgentsByServiceTypes(getAgent(), dstTypes);
                         actualStatus = !actualStatus;
-                        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-                        msg.setContent(String.valueOf(actualStatus));
-                        for (DFAgentDescription dstAgent : dstAgents) {
-                            msg.addReceiver(dstAgent.getName());
+                        if (actualStatus) {
+                            List<DFAgentDescription> dstAgents = DFUtils.findAgentsByServiceTypes(getAgent(), dstTypes);
+                            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                            msg.setContent(String.valueOf(actualStatus));
+                            for (DFAgentDescription dstAgent : dstAgents) {
+                                msg.addReceiver(dstAgent.getName());
+                            }
+                            getAgent().send(msg);
                         }
-                        getAgent().send(msg);
                     }
                 } catch (Exception e) {
                     System.out.println("ERROR MOV");
